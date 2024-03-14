ca18fc8dff7bea0a00479ab156a875a654441ba9
manuela.sventnickas <manu.svent@gmail.com>
2024-03-13 21:34:16 -0300

    Commit

--- src/main/java/com/example/demo/resource/FuncionarioController.java
+++ src/main/java/com/example/demo/resource/FuncionarioController.java
@@ -3,9 +3,14 @@ package com.example.demo.resource;
 import com.example.demo.model.Funcionario;
 import com.example.demo.repository.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
+
+
 
 import java.util.List;
 
@@ -22,8 +27,11 @@ public class FuncionarioController {
     }
 
     @GetMapping
-    public ResponseEntity findAll(){
-        List<Funcionario> funcionarios = repository.findAll();
-        return ResponseEntity.ok(FuncionarioDTO.fromEntityList(funcionarios));
+    public ResponseEntity findAll(@RequestParam(defaultValue = "9") int page,
+                                  @RequestParam(defaultValue = "10") int size){
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Funcionario> funcionarioPage = repository.findAll(pageable);
+        Page<FuncionarioDTO> funcionarioDTOPage = funcionarioPage.map(FuncionarioDTO::fromEntity);
+                return ResponseEntity.ok(funcionarioDTOPage);
     }
 }
